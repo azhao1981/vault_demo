2de14aaa4f1d96fbfa3caa152f83f16be6b18b35
weiz <azhao.1981@gmail.com>
2021-07-26 14:18:57 +0800

    hello world

---
+++ REAMDE.md
@@ -0,0 +1,11 @@
+# readme
+
+## TODO
+1. 连接vault,显示 dbusername
+   http://localhost:8080/
+2. 连接db,使用原始的 datasource
+    2.1. 输入参数，插入数据
+    2.2. 列表
+3. 用 vault的值来代替 datasource
+
+### vault config
--- src/main/java/com/udesk/vault_demo/VaultDemoApplication.java
+++ src/main/java/com/udesk/vault_demo/VaultDemoApplication.java
@@ -2,12 +2,20 @@ package com.udesk.vault_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class VaultDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VaultDemoApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String home() {
+		return "hello world";
 	}
 
 }
